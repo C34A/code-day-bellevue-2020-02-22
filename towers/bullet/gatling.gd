6f09d3dab45bebe266a4d013860e6211875de550
Theo Marshall <theo@seelye.net>
2020-02-22 16:25:10 -0800

    made things work

--- towers/bullet/gatling.gd
+++ towers/bullet/gatling.gd
@@ -1,37 +1,77 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var spawn_bullets: bool = false
-var target_point: Vector3 = Vector3(0, 0, 1)
+var target_point: Vector2 = Vector2(0, 1)
+const COOLDOWN: float = 0.25
+var shot_cooldown: float = 0.25
+var disabled: bool = false
+
+onready var animation = $AnimationPlayer
+onready var turret_top = $tower/top
+onready var bullet_point = $tower/top/bullet_point
 
 const ROTSPEED: float = 3.0
 
-# Called when the node enters the scene tree for the first time.
+const GHOST_MATERIAL = preload("res://towers/bullet/ghost.tres")
+const GHOST_COLLISION_MATERIAL = preload("res://towers/bullet/ghost-collision.tres")
+
+const bullet_scene = preload("res://towers/bullet/projectile.tscn")
+
 func _ready():
 	$AnimationPlayer.get_animation("default").set_loop(true)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("TEST-shoot"):
-		start_shooting()
-	elif Input.is_action_just_pressed("TEST-stopshoot"):
-		stop_shooting()
-	
-	var target_angle: float = global_transform.origin.angle_to(target_point)
-	print($tower/top.global_transform.basis.get_euler().y - target_angle)
-	
-#	$tower/top.global_transform.basis = $tower/top.global_transform.basis.rotated(Vector3.UP, ROTSPEED * delta)
-	$tower/top.rotate_y(ROTSPEED * delta * sign(target_angle - $tower/top.global_transform.basis.get_euler().y))
+	if not disabled:
+		var pos: Vector2 = Vector2(global_transform.origin.x, global_transform.origin.z)
+		var target_angle: float = atan2((target_point - pos).x, (target_point - pos).y)
+##		print($tower/top.global_transform.basis.get_euler().y - target_angle)
+#		print(target_point)
+#		print(target_angle)
+		
+	#	$tower/top.global_transform.basis = $tower/top.global_transform.basis.rotated(Vector3.UP, ROTSPEED * delta)
+		$tower/top.rotate_y(ROTSPEED * delta * sign(target_angle - turret_top.global_transform.basis.get_euler().y))
+		
+		if spawn_bullets:
+			if shot_cooldown <= 0:
+				var projectile = bullet_scene.instance()
+				add_child(projectile)
+				projectile.global_transform = bullet_point.global_transform
+#				projectile.global_rotate(Vector3.UP, bullet_point.global_transform.basis.get_euler().y)
+				print(bullet_point.global_transform.basis.get_euler())
+				shot_cooldown = COOLDOWN
+			else:
+				shot_cooldown -= delta
+
+func point_towards(target: Spatial):
+	target_point = Vector2(target.global_transform.origin.x, target.global_transform.origin.z)
+
 
 func start_shooting():
 	spawn_bullets = true
 	#rotate barrels
-	$AnimationPlayer.play("default")
-	
+	animation.play("default")
+
+
+func set_all_materials(material):
+	$tower.set_material_override(material)
+	$tower/top.set_material_override(material)
+	$tower/top/barrels.set_material_override(material)
+	$tower/top/base.set_material_override(material)
+
+
+func make_ghost():
+	set_all_materials(GHOST_MATERIAL)
+
+
+func make_ghost_collision():
+	set_all_materials(GHOST_COLLISION_MATERIAL)
+
+
+func make_not_ghost():
+	set_all_materials(null)
+
+
 func stop_shooting():
 	spawn_bullets = false
-	$AnimationPlayer.stop()
+	animation.stop()
--- towers/bullet/projectile.gd
+++ towers/bullet/projectile.gd
@@ -4,7 +4,7 @@ extends KinematicBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed: float = 2.0
+var speed: float = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +13,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_and_slide(Vector3.FORWARD, Vector3.UP)
+	move_and_collide(global_transform.basis * Vector3.UP * speed * delta)
+#	print(Vector3.FORWARD * speed * delta)
