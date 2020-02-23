f54840b4f3c7f43f8025513de58ffe2d63fd6032
Theo Marshall <theo@seelye.net>
2020-02-22 22:23:26 -0800

    fixed tree again

--- scripts/Towers.gd
+++ scripts/Towers.gd
@@ -11,5 +11,5 @@ func add_tower(tower: Spatial, position: Vector3):
 	add_child(tower);
 	
 func _ready():
-	add_tower(Gatling.instance(), Vector3.ZERO);
+	add_tower(Base.instance(), Vector3.ZERO);
 	pass
