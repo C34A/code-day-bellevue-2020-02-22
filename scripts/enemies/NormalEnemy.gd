8bd2878999d83ab4bc0465abc8de3932474ec506
Theo Marshall <theo@seelye.net>
2020-02-22 18:09:04 -0800

    6pm

--- scripts/enemies/NormalEnemy.gd
+++ scripts/enemies/NormalEnemy.gd
@@ -1,7 +1,9 @@
 extends KinematicBody
 
+const SPEED = 1.0
+
 func _ready():
 	pass
 
 func _process(delta):
-	move_and_collide(Vector3.FORWARD);
+	move_and_collide(Vector3.FORWARD * delta * SPEED);
